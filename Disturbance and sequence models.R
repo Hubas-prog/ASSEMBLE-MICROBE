f011350faf7f7afdd654171eca9ce031e1675e08
James Rimmer <jr49@st-andrews.ac.uk>
2020-05-28 11:36:28 +0100

    Remove redundant code in model specification

--- Disturbance and sequence models.R
+++ Disturbance and sequence models.R
@@ -25,7 +25,7 @@ chlmodel <- stan_glm(formula = Chlorophyll ~ Seq + Disturb,
 
   ### Alpha ###
 
-alphamodel <- chlmodel <- stan_lmer(formula = Alpha ~ Seq + + Disturb + Time + (1 | repli), 
+alphamodel <- stan_lmer(formula = Alpha ~ Seq + + Disturb + Time + (1 | repli), 
                                    data = PAM_FULL, adapt_delta = 0.9999, iter = 3000, prior_intercept = normal(0.2, 5, autoscale = F),
                                    prior = normal(location = c(0,0,0), scale = c(0.086,0.086,0.104), autoscale = F),
                                    prior_aux = exponential(rate = 29, autoscale = F), prior_covariance = 
@@ -33,7 +33,7 @@ alphamodel <- chlmodel <- stan_lmer(formula = Alpha ~ Seq + + Disturb + Time + (
 
   ### Ek ###
 
-Ekmodel <- chlmodel <- stan_lmer(formula = Ek ~ Seq + + Disturb + Time + (1 | repli), 
+Ekmodel <- stan_lmer(formula = Ek ~ Seq + + Disturb + Time + (1 | repli), 
                                     data = PAM_FULL, adapt_delta = 0.9999, iter = 3000, prior_intercept = normal(200, 50, autoscale = F),
                                     prior = normal(location = c(0,0,0), scale = c(120.51,120.51,145.53), autoscale = F),
                                     prior_aux = exponential(rate = 0.021, autoscale = F), prior_covariance = 
@@ -41,7 +41,7 @@ Ekmodel <- chlmodel <- stan_lmer(formula = Ek ~ Seq + + Disturb + Time + (1 | re
 
   ### rETRmax ###
         
-Ekmodel <- chlmodel <- stan_lmer(formula = rETRmax ~ Seq * Disturb + Time + (1 | repli), 
+Ekmodel <- stan_lmer(formula = rETRmax ~ Seq * Disturb + Time + (1 | repli), 
                                  data = PAM_FULL, adapt_delta = 0.9999, iter = 3000, prior_intercept = normal(60, 20, autoscale = F),
                                  prior = normal(location = c(0,0,0, 0), scale = c(18.67,18.67,22.54, 18.67), autoscale = F),
                                  prior_aux = exponential(rate = 0.13, autoscale = F), prior_covariance = 
@@ -49,7 +49,7 @@ Ekmodel <- chlmodel <- stan_lmer(formula = rETRmax ~ Seq * Disturb + Time + (1 |
 
   ### Fv/Fm ###
 
-Fv/Fmmodel <- chlmodel <- stan_lmer(formula = Fv.Fm ~ Seq + + Disturb + Time + (1 | repli), 
+Fv/Fmmodel <- stan_lmer(formula = Fv.Fm ~ Seq + + Disturb + Time + (1 | repli), 
                                  data = PAM_FULL, adapt_delta = 0.9999, iter = 3000, prior_intercept = normal(0.6, 10, autoscale = F),
                                  prior = normal(location = c(0,0,0), scale = c(0.21,0.21,0.25), autoscale = F),
                                  prior_aux = exponential(rate = 12, autoscale = F), prior_covariance = 
